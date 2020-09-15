072cfa58e8b07cb1e71d85a0bb605282b27ca5ee
oivashchenko20 <53085700+oivashchenko20@users.noreply.github.com>
2020-09-15 20:38:01 +0300

    first start

---
+++ src/main/java/com/example/demoRestProject/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.demoRestProject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String firstStart() {
+        return "Hello";
+    }
+}
