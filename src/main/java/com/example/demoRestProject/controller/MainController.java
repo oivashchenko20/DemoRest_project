fa81240ae1d05cbeb113b86359ba98ab0659f0ec
Oleksandr Ivashchenko <oivashchenko@ascendix.net>
2020-09-18 15:29:53 +0300

    createUserFindAll

--- src/main/java/com/example/demoRestProject/controller/MainController.java
+++ src/main/java/com/example/demoRestProject/controller/MainController.java
@@ -1,13 +1,27 @@
 package com.example.demoRestProject.controller;
 
+import com.example.demoRestProject.entity.User;
+import com.example.demoRestProject.service.serviceImpl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class MainController {
+    private final UserServiceImpl userServiceImpl;
+
+    @Autowired
+    public MainController(UserServiceImpl userServiceImpl) {
+        this.userServiceImpl = userServiceImpl;
+    }
+
 
     @RequestMapping("/")
-    public String firstStart() {
-        return "Hello";
+    public List<User> findUsers() {
+        return userServiceImpl.findAllUsers();
     }
 }
---
+++ src/main/java/com/example/demoRestProject/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.demoRestProject.service;
+
+import com.example.demoRestProject.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> findAllUsers();
+}
